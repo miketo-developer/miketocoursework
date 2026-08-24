b6ba584b51928218a9825872cc1911526fef4509
Miketo <miketo.developer@gmail.com>
2026-08-24 05:31:43 -0600

    Preconfiguraciones antes del deploy

--- app/sw.ts
+++ app/sw.ts
@@ -1,11 +1,15 @@
 /// <reference lib="webworker" />
 import { defaultCache } from "@serwist/next/worker";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
-import type { PrecacheEntry } from "serwist";
 
-declare const self: ServiceWorkerGlobalScope & {
-  __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-};
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
+}
+
+declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
