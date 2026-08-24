c60888f7afccf2a935cfdb7da289d202154aa722
Miketo <miketo.developer@gmail.com>
2026-08-24 06:57:10 -0600

    Cambio de num de Whats

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -1,6 +1,6 @@
 // lib/whatsapp.ts
 // Pon aquí tu número con lada, sin + ni espacios. Ej: México 52 + número
-export const WHATSAPP_NUMBER = "5215542445352"; 
+export const WHATSAPP_NUMBER = "5215513829007"; 
 
 export const getWaLink = (text: string) => {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(text)}`;
